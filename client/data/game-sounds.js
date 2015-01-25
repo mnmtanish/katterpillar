83792b1ebe02e6a7a41745ef9c8b4d812b80cad2
Muhammed Thanish <mnmtanish@gmail.com>
2015-01-26 03:35:08 +0530

    Support mobile browsers

--- client/data/game-sounds.js
+++ client/data/game-sounds.js
@@ -4,18 +4,22 @@ $(function () {
   GameSounds.enabled = new ReactiveVar(true);
 
   GameSounds.step = new buzz.sound( "/sounds/branch_break", {
-    formats: [ "ogg", "mp3", "aac" ]
+    formats: ["mp3"]
+    // formats: [ "ogg", "mp3", "aac" ]
   });
 
   GameSounds.eat = new buzz.sound( "/sounds/glass", {
-    formats: [ "ogg", "mp3", "aac" ]
+    formats: ["mp3"]
+    // formats: [ "ogg", "mp3", "aac" ]
   });
 
   GameSounds.hit = new buzz.sound( "/sounds/light_bulb_breaking", {
-    formats: [ "ogg", "mp3", "aac" ]
+    formats: ["mp3"]
+    // formats: [ "ogg", "mp3", "aac" ]
   });
 
   GameSounds.background = new buzz.sound( "/sounds/background", {
-    formats: [ "ogg", "mp3", "aac" ]
+    formats: ["mp3"]
+    // formats: [ "ogg", "mp3", "aac" ]
   });
 });
